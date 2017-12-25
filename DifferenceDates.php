6f9a48fe45e77c7970d4866fa0b771c2d2ad172d
Rinat Safin <safinrinat87@gmail.com>
2017-12-25 18:34:10 +0200

    modify calculate difference method

--- DifferenceDates.php
+++ DifferenceDates.php
@@ -1,8 +1,9 @@
 <?php
 /*
  * http://php.net/manual/en/reserved.classes.php
+ * http://php.net/manual/ru/datetime.diff.php
  * */
-class DifferenceDates extends \stdClass
+class DifferenceDates
 {
     public $dateOne;
     public $dateTwo;
@@ -12,11 +13,11 @@ class DifferenceDates extends \stdClass
     public $monthsEnd;
     public $daysStart;
     public $daysEnd;
-    public $yearsBetween;
-    public $monthsBetween;
-    public $daysBetween;
-    public $totalDaysBetween;
-    public $invert = false;
+    protected $yearsBetween;
+    protected $monthsBetween;
+    protected $daysBetween;
+    protected $totalDaysBetween;
+    private $invert = false;
 
     /**
      * DifferenceDates constructor.
@@ -27,19 +28,61 @@ class DifferenceDates extends \stdClass
     {
         $this->dateOne = explode("-",trim($dateOne));
         $this->dateTwo = explode("-", trim($dateTwo));
-        $this->invert = $this->checkSmallYear();
+        $this->invert = $this->checkSmallYear($dateOne, $dateTwo);
     }
 
-    public function checkSmallYear() {
-        if (!$this->dateOne || !$this->dateTwo) throw new Exception("Dates format is not correct! <br> Try YYYY-MM-DD");
-        return intval($this->dateOne[0]) > intval($this->dateTwo[0]) ? false : true;
+    /**
+     * @return bool
+     * @throws Exception
+     */
+    public function checkSmallYear($dateOne, $dateTwo)
+    {
+        if (!$this->dateOne || !$this->dateTwo) throw new Exception("Dates format is not correct! <br> Try 'YYYY-MM-DD'");
+        return ($dateOne > $dateTwo) || intval($this->dateOne[0]) > intval($this->dateTwo[0]) ? false : true;
     }
 
-    public function calculateDifference() {
-        $this->daysStart = $this->invert ? $this->dateTwo[0] : $this->dateOne[0];
+    /**
+     *
+     * @return string
+     */
+    public function calculateDifference()
+    {
+        if ($this->invert) {
+            $tmpDate = $this->dateOne;
+            $this->dateOne = $this->dateTwo;
+            $this->dateTwo = $tmpDate;
+        }
+        $this->yearsStart = (int) $this->dateOne[0];
+        $this->yearsEnd = (int) $this->dateOne[0];
+        $this->monthsStart = strlen($this->dateOne[1]) > 2 ? (int) substr($this->dateOne[1], 0, 2) : (int) $this->dateOne[1];
+        $this->monthsEnd = strlen($this->dateTwo[1]) > 2 ? (int) substr($this->dateTwo[1], 0, 2) : (int) $this->dateTwo[1];
+        $this->daysStart = strlen($this->dateOne[2]) > 2 ? (int) substr($this->dateOne[2], 0, 2) : (int) $this->dateOne[2];
+        $this->daysEnd = strlen($this->dateTwo[2]) > 2 ? (int) substr($this->dateTwo[2], 0, 2) : (int) $this->dateTwo[2];
+
+        if ($this->monthsStart > 12 && $this->monthsEnd > 12 && $this->daysStart > 31 && $this->daysEnd > 31) {
+            return "ERROR: NOT VALID FORMAT! TRY 'YYYY-MM-DD'";
+        }
+
+        if ($this->daysEnd > $this->daysStart) {
+            if ($this->monthsEnd > $this->monthsStart) {
+                $this->yearsBetween = --$this->yearsStart - $this->yearsEnd;
+                $this->monthsBetween = ($this->monthsStart + 12) - $this->monthsEnd;
+            }
+        }
+
         $this->yearsBetween;
         $this->monthsBetween;
         $this->daysBetween;
         $this->totalDaysBetween;
+    }
+
+    private function getDayInMonth($month, $year)
+    {
+        return $month == 2 ? ($year % 4 ? 28 : ($year % 100 ? 29 : ($year %400 ? 28 : 29))) : (($month - 1) % 7 % 2 ? 30 : 31);
+    }
+
+    private function isLeapYear($year)
+    {
+        return ((($year % 4) == 0) && ((($year % 100) != 0) || (($year % 400) == 0)));
     }
 }
